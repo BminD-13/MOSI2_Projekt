febe5b142d7ebe4680bab6940cae447f4efbbde2
BminD-13 <benjamind1995@googlemail.com>
2023-05-18 15:52:26 +0200

    Parameter fuer Basismodell implementiert

---
+++ Tourismusdynamik.m
@@ -0,0 +1,10 @@
+%% Parameter
+werbeEinfluss = 5;          % 1/jahr
+verlustRate = 0.5;          % 1/jahr
+umsatzanteilFuerUmwelt = 0; % 1
+verbrauchsRate              % 1/jahr
+verbesserungsRate = 1;      % 1/jahr
+regenerationsZeit = 10;     % 1/jahr
+
+%% Diff Gleichungen
+
