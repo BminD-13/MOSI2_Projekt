69d6536ba13a40c0e08de71f6b73d89ac2e4fa6d
BminD-13 <benjamind1995@googlemail.com>
2023-05-20 01:41:22 +0200

    DiffGl vervollstaendigt funktioniert nich als system ...

--- Tourismusdynamik.m
+++ Tourismusdynamik.m
@@ -1,32 +1,53 @@
-%% Parameter
-werbeEinfluss = 5;          % [1/jahr]
-verlustRate = 0.5;          % [1/jahr]
-umsatzanteilFuerUmwelt = 0; % [1]
-verbrauchsRate = 0.1;       % [1/jahr]
-verbesserungsRate = 1;      % [1/jahr]
-regenerationsZeit = 10;     % [1/jahr]
-kapazitaet = 1;             % [1]
+clc
+clear
+close all
+
+%% p Parameter
+pWerbeEinfluss = 5;          % [1/jahr]
+pVerlustRate = 0.5;          % [1/jahr]
+pUmsatzanteilFuerUmwelt = 0; % [1]
+pVerbrauchsRate = 0.1;       % [1/jahr]
+pVerbesserungsRate = 1;      % [1/jahr]
+pRegenerationsZeit = 10;     % [1/jahr]
+pKapazitaet = 1;             % [1]
+
 
 %% Simulationsparameter
-initialTime = 0;            % [jahr]
-finalTime = 20;             % [jahr]
+timeInitial = 0;            % [jahr]
+timeFinal = 20;             % [jahr]
 timeStep = 0.05;            % [jahr]
 
+timeVektor = timeInitial : timeStep : timeFinal;
+
+
 %% Dynamik
-preisniveau = touristen;                                        % [1]
-attraktivitaet = umweltQualitaet/(touristen * preisniveau);     % [1]
-umsatz = touristen;                                             % [1]
+attraktivitaet =@(t, touristen, umweltQualitaet) umweltQualitaet / (touristen * preisniveau(t)); % [1]
+preisniveau =@(t, touristen) touristen;                                        % [1]
+umsatz =@(t, touristen) touristen;                                             % [1]
 
 % Touristen
-touristenZuwachs = attraktivitaet * werbeEinfluss * touristen;  % [1/jahr]
-touristenVerlust = verlustRate * touristen;                     % [1/jahr]
-
-touristen = integralueber(+ touristenZuwachs - touristenVerlust, 1);% [1]
+touristenZuwachs =@(t, touristen, umweltQualitaet) attraktivitaet(t, touristen, umweltQualitaet) * pWerbeEinfluss * touristen;  % [1/jahr]
+touristenVerlust =@(t, touristen) pVerlustRate * touristen;                     % [1/jahr]
+% der inhalt der klammer muss noch integriert werden !!!!!
+touristen_ =@(t, touristen, umweltQualitaet) (touristenZuwachs(t, touristen, umweltQualitaet) - touristenVerlust(t, touristen));% [1] (+ touristenZuwachs - touristenVerlust, 1)
 
 % Umwelt
-umweltAusgaben = umsatz * umsatzanteilFuerUmwelt / 100;         % [1]
-umweltVerbrauch = umsatz * umweltQualitaet * verbrauchsRate;    % [1/jahr]
-umweltVerbesserung = umweltAusgaben * verbesserungsRate;        % [1/jahr]
-umweltErneuerung = (umweltQualitaet / regenerationsZeit) * (1 - umweltQualitaet / kapazitaet); % [1/jahr]
+umweltAusgaben =@(t, touristen) umsatz(t, touristen) * pUmsatzanteilFuerUmwelt / 100;         % [1]
+umweltVerbrauch =@(t, umweltQualitaet) umsatz(t) * umweltQualitaet * pVerbrauchsRate;    % [1/jahr]
+umweltVerbesserung =@(t) umweltAusgaben(t) * pVerbesserungsRate;        % [1/jahr]
+umweltErneuerung =@(t, umweltQualitaet) (umweltQualitaet / pRegenerationsZeit) * (1 - umweltQualitaet / pKapazitaet); % [1/jahr]
+% der inhalt der klammer muss noch integriert werden !!!!!
+umweltQualitaet_ =@(t, touristen, umweltQualitaet) (umweltErneuerung(t, umweltQualitaet) + umweltVerbesserung(t) - umweltVerbrauch(t, umweltQualitaet)); % [1]
 
-umweltQualitaet = integralueber(umweltErneuerung + umweltVerbesserung - umweltVerbrauch); % [1]
+% Diff'gleichungen mit ode45 loesen
+touristen       = ode45(touristen_, timeVektor,1);
+umweltQualitaet = ode45(umweltQualitaet_, timeVektor,1);
+
+%% Plot
+
+subplot(3,1,1)
+    plot(timeVektor, touristen);
+subplot(3,1,2)
+    plot(timeVektor, umweltQualitaet);
+
+
