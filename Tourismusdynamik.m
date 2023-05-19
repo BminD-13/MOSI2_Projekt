df9246669b54481da374e820b10b811562fe7e85
BminD-13 <benjamind1995@googlemail.com>
2023-05-19 18:03:37 +0200

    Parameter Zeit hinzugefuegt Dynamik Implementiert (nicht funktional)

--- Tourismusdynamik.m
+++ Tourismusdynamik.m
@@ -1,10 +1,26 @@
 %% Parameter
-werbeEinfluss = 5;          % 1/jahr
-verlustRate = 0.5;          % 1/jahr
-umsatzanteilFuerUmwelt = 0; % 1
-verbrauchsRate              % 1/jahr
-verbesserungsRate = 1;      % 1/jahr
-regenerationsZeit = 10;     % 1/jahr
+werbeEinfluss = 5;          % [1/jahr]
+verlustRate = 0.5;          % [1/jahr]
+umsatzanteilFuerUmwelt = 0; % [1]
+verbrauchsRate              % [1/jahr]
+verbesserungsRate = 1;      % [1/jahr]
+regenerationsZeit = 10;     % [1/jahr]
 
-%% Diff Gleichungen
+%% Simulationsparameter
+initialTime = 0;            % [jahr]
+finalTime = 20;             % [jahr]
+timeStep = 0.05;            % [jahr]
 
+%% Dynamik
+preisniveau = touristen;    % [1]
+attraktivitaet = umweltQualitaet/(touristen * preisniveau);  % [1]
+touristenZuwachs = attraktivitaet * werbeEinfluss * touristen; % [1/jahr]
+touristenVerlust = verlustRate * touristen; % [1/jahr]
+umsatz = touristen; % [1]
+ausgabenFuerUmwelt = umsatz * umsatzanteilFuerUmwelt / 100; % [1]
+umweltVerbrauch = umsatz * umweltQualitaet * verbrausRate;
+umweltVerbesserung = ausgabenFuerUmwelt * verbesserungsRate;
+umweltErneuerung = (umweltQualitaet / regenerationsZeit) * (1 - umweltQualitaet / kapazitaet);
+umweltQualitaet = integralueber(umweltErneuerung + umweltVerbesserung - umweltVerbrauch); % [1]
+
+touristen = integralueber(+ touristenZuwachs - touristenVerlust, 1);% [1]
