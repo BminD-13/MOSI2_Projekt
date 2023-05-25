b0e333163743a353b76101c5f1e7cd953b8da2ef
BminD-13 <benjamind1995@googlemail.com>
2023-05-25 15:47:18 +0200

    Simulink Modell -Funktionen ausprogrammiert -Modell fertiggestellt -muesste funktionieren
    Matlab Script
    -Aufruf des Modells
    -plot der Ergebnisse
        -> plottet noch nicht ...

--- Tourismusdynamik.m
+++ Tourismusdynamik.m
@@ -19,35 +19,46 @@ timeStep = 0.05;            % [jahr]
 
 timeVektor = timeInitial : timeStep : timeFinal;
 
+%% Simulink Modell
+
+open("TourismusdynamikSim")
+modelData = sim("TourismusdynamikSim");
+
+%% Plot Simulink Modell
+
+plot(modelData.tout, modelData.touristen.Data)
+% plot(sim.umweltQualitaet.Time, sim.umweltQualitaet.Data)
+
 
 %% Dynamik
-attraktivitaet =@(t, touristen, umweltQualitaet) umweltQualitaet / (touristen * preisniveau(t)); % [1]
-preisniveau =@(t, touristen) touristen;                                        % [1]
-umsatz =@(t, touristen) touristen;                                             % [1]
 
-% Touristen
-touristenZuwachs =@(t, touristen, umweltQualitaet) attraktivitaet(t, touristen, umweltQualitaet) * pWerbeEinfluss * touristen;  % [1/jahr]
-touristenVerlust =@(t, touristen) pVerlustRate * touristen;                     % [1/jahr]
-% der inhalt der klammer muss noch integriert werden !!!!!
-touristen_ =@(t, touristen, umweltQualitaet) (touristenZuwachs(t, touristen, umweltQualitaet) - touristenVerlust(t, touristen));% [1] (+ touristenZuwachs - touristenVerlust, 1)
-
-% Umwelt
-umweltAusgaben =@(t, touristen) umsatz(t, touristen) * pUmsatzanteilFuerUmwelt / 100;         % [1]
-umweltVerbrauch =@(t, umweltQualitaet) umsatz(t) * umweltQualitaet * pVerbrauchsRate;    % [1/jahr]
-umweltVerbesserung =@(t, touristen) umweltAusgaben(t, touristen) * pVerbesserungsRate;        % [1/jahr]
-umweltErneuerung =@(t, umweltQualitaet) (umweltQualitaet / pRegenerationsZeit) * (1 - umweltQualitaet / pKapazitaet); % [1/jahr]
-% der inhalt der klammer muss noch integriert werden !!!!!
-umweltQualitaet_ =@(t, touristen, umweltQualitaet) (umweltErneuerung(t, umweltQualitaet) + umweltVerbesserung(t) - umweltVerbrauch(t, umweltQualitaet)); % [1]
-
-% Diff'gleichungen mit ode45 loesen
-touristen       = ode45(touristen_, timeVektor,1);
-umweltQualitaet = ode45(umweltQualitaet_, timeVektor,1);
+% attraktivitaet =@(t, touristen, umweltQualitaet) umweltQualitaet / (touristen * preisniveau(t)); % [1]
+% preisniveau =@(t, touristen) touristen;                                        % [1]
+% umsatz =@(t, touristen) touristen;                                             % [1]
+% 
+% % Touristen
+% touristenZuwachs =@(t, touristen, umweltQualitaet) attraktivitaet(t, touristen, umweltQualitaet) * pWerbeEinfluss * touristen;  % [1/jahr]
+% touristenVerlust =@(t, touristen) pVerlustRate * touristen;                     % [1/jahr]
+% % der inhalt der klammer muss noch integriert werden !!!!!
+% touristen_ =@(t, touristen, umweltQualitaet) (touristenZuwachs(t, touristen, umweltQualitaet) - touristenVerlust(t, touristen));% [1] (+ touristenZuwachs - touristenVerlust, 1)
+% 
+% % Umwelt
+% umweltAusgaben =@(t, touristen) umsatz(t, touristen) * pUmsatzanteilFuerUmwelt / 100;         % [1]
+% umweltVerbrauch =@(t, touristen, umweltQualitaet) umsatz(t, touristen) * umweltQualitaet * pVerbrauchsRate;    % [1/jahr]
+% umweltVerbesserung =@(t, touristen) umweltAusgaben(t, touristen) * pVerbesserungsRate;        % [1/jahr]
+% umweltErneuerung =@(t, umweltQualitaet) (umweltQualitaet / pRegenerationsZeit) * (1 - umweltQualitaet / pKapazitaet); % [1/jahr]
+% % der inhalt der klammer muss noch integriert werden !!!!!
+% umweltQualitaet_ =@(t, touristen, umweltQualitaet) (umweltErneuerung(t, umweltQualitaet) + umweltVerbesserung(t) - umweltVerbrauch(t, touristen, umweltQualitaet)); % [1]
+% 
+% % Diff'gleichungen mit ode45 loesen
+% touristen       = ode45(touristen_, timeVektor,1);
+% umweltQualitaet = ode45(umweltQualitaet_, timeVektor,1);
 
 %% Plot
 
-subplot(3,1,1)
-    plot(timeVektor, touristen);
-subplot(3,1,2)
-    plot(timeVektor, umweltQualitaet);
+% subplot(3,1,1)
+%     plot(timeVektor, touristen);
+% subplot(3,1,2)
+%     plot(timeVektor, umweltQualitaet);
 
 
