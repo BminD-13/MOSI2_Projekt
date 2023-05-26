e06315a89dd3466e79f181d09cb3e8044d792473
BminD-13 <benjamind1995@googlemail.com>
2023-05-26 13:21:37 +0200

    Model Lauffaehig plottet Ergenisse

--- Tourismusdynamik.m
+++ Tourismusdynamik.m
@@ -11,7 +11,6 @@ pVerbesserungsRate = 1;      % [1/jahr]
 pRegenerationsZeit = 10;     % [1/jahr]
 pKapazitaet = 1;             % [1]
 
-
 %% Simulationsparameter
 timeInitial = 0;            % [jahr]
 timeFinal = 20;             % [jahr]
@@ -24,41 +23,11 @@ timeVektor = timeInitial : timeStep : timeFinal;
 open("TourismusdynamikSim")
 modelData = sim("TourismusdynamikSim");
 
-%% Plot Simulink Modell
-
-plot(modelData.tout, modelData.touristen.Data)
-% plot(sim.umweltQualitaet.Time, sim.umweltQualitaet.Data)
-
-
-%% Dynamik
-
-% attraktivitaet =@(t, touristen, umweltQualitaet) umweltQualitaet / (touristen * preisniveau(t)); % [1]
-% preisniveau =@(t, touristen) touristen;                                        % [1]
-% umsatz =@(t, touristen) touristen;                                             % [1]
-% 
-% % Touristen
-% touristenZuwachs =@(t, touristen, umweltQualitaet) attraktivitaet(t, touristen, umweltQualitaet) * pWerbeEinfluss * touristen;  % [1/jahr]
-% touristenVerlust =@(t, touristen) pVerlustRate * touristen;                     % [1/jahr]
-% % der inhalt der klammer muss noch integriert werden !!!!!
-% touristen_ =@(t, touristen, umweltQualitaet) (touristenZuwachs(t, touristen, umweltQualitaet) - touristenVerlust(t, touristen));% [1] (+ touristenZuwachs - touristenVerlust, 1)
-% 
-% % Umwelt
-% umweltAusgaben =@(t, touristen) umsatz(t, touristen) * pUmsatzanteilFuerUmwelt / 100;         % [1]
-% umweltVerbrauch =@(t, touristen, umweltQualitaet) umsatz(t, touristen) * umweltQualitaet * pVerbrauchsRate;    % [1/jahr]
-% umweltVerbesserung =@(t, touristen) umweltAusgaben(t, touristen) * pVerbesserungsRate;        % [1/jahr]
-% umweltErneuerung =@(t, umweltQualitaet) (umweltQualitaet / pRegenerationsZeit) * (1 - umweltQualitaet / pKapazitaet); % [1/jahr]
-% % der inhalt der klammer muss noch integriert werden !!!!!
-% umweltQualitaet_ =@(t, touristen, umweltQualitaet) (umweltErneuerung(t, umweltQualitaet) + umweltVerbesserung(t) - umweltVerbrauch(t, touristen, umweltQualitaet)); % [1]
-% 
-% % Diff'gleichungen mit ode45 loesen
-% touristen       = ode45(touristen_, timeVektor,1);
-% umweltQualitaet = ode45(umweltQualitaet_, timeVektor,1);
-
-%% Plot
-
-% subplot(3,1,1)
-%     plot(timeVektor, touristen);
-% subplot(3,1,2)
-%     plot(timeVektor, umweltQualitaet);
-
-
+%% Plot Ergebnisse Simulink Modell 
+hold on
+    plot(modelData.tout, modelData.touristen.Data)
+    plot(modelData.tout, modelData.umweltQualitaet.Data)
+    title("Tourismusdynamik")
+    xlabel("Jahre")
+    legend("Touristen","Umweltqualitaet")
+hold off
